4176d643f47715f0348a957af0f2a89fe5b759c3
Lucas Morley <lnm48@csb.pitt.edu>
2021-04-25 22:37:31 -0400

    documented scripts

--- rmsf.py
+++ rmsf.py
@@ -1,11 +1,48 @@
+'''
+Calculates the average RMSF for each residue in the trajectory using
+MDAnalysis's RMSF method.
+
+Output:
+If called using shortcut for multiple residues, returned is a single csv called rmsfs.csv containg
+the resname, resid, and trajectory number
+e.g.
+resname | resid | traj_1 | resid | traj_2 ...
+LEU     |     1 |  1.272 |     1 |  1.410
+...
+
+If called using a single trajectory, returned is a csv called rmsfs.csv containing resid, resname
+and RMSF
+
+resid | resname | RMSF
+    1 | LEU     | 1.27
+...
+
+Running the script:
+Since calculating RMSF is not a very intense calculation, it may be useful to just
+produce a single csv for all trajectories. If your trajetories are .dcd and have 
+a naming scheme of prefix_number.dcd e.g. traj_1.dcd you can use the shortcut call.
+
+python rmsf.py MDAnalysis_supported_topology trajectory_prefix total_number_of_trajs_starting_at_1
+e.g. 
+python rmsf.py FXN_R165N_complex.prmtop FXN_R165N_compex_ 10 
+
+You can also call the script using a single trajectory:
+
+python rmsfs.py MDAnalysis_supported_topology MDAnalysis_supported_trajectory
+e.g.  
+python rmsf.py FXN_R165N_complex.prmtop FXN_R165N_compex_1.dcd
+'''
 import sys
 import MDAnalysis
 import pandas as pd
 from pandas import DataFrame
-import numpy as np
+import numpy as np # numpy 1.16.6
 from MDAnalysis.analysis import align
 from MDAnalysis.analysis.rms import RMSF
 
+'''
+Loads trajectory(s) into trajs
+'''
 def getTrajs():
 	top = str(sys.argv[1])
 	dcd_prefix = str(sys.argv[2])
@@ -23,6 +60,10 @@ def getTrajs():
 
 	return trajs, traj
 
+
+'''
+Calculates the avergae RMSF for each residue in the trajectory
+'''
 def get_rmsf(t,label=None):
 	sel = 'protein and not name H*'
 	prot = t.select_atoms(sel)
@@ -38,20 +79,29 @@ def get_rmsf(t,label=None):
     	return ret
 
 
+'''
+Creates and returns the final dataframe to become rmsfs.csv
+'''
 def rmsf_df(trajs, traj):
  	rmsfs = DataFrame()
  	protein = traj.select_atoms('protein')
 
  	rmsfs['resname'] = protein.residues.resnames
- 	# rmsfs['res_num'] = protein.residues.resids
 
  	for i in range(len(trajs)):
 		print('\tCalculating rmsf for traj {}'.format(i + 1))
     		rmsf = get_rmsf(trajs[i])
-    		col_name = 'traj_%s' % str(i + 1)
+		if (len(trajs) == 1):
+			col_name = 'RMSF'
+		else:
+    			col_name = 'traj_%s' % str(i + 1)
     		rmsf = rmsf.groupby(['resid']).mean().reset_index().rename(columns={'rmsf': col_name})
     		rmsfs = pd.concat([rmsfs, rmsf], axis=1)
-   	return rmsfs
+   	if (len(trajs) == 1):
+		return rmsfs.set_index('resid')
+	else:
+		return rmsfs
+
 
 if __name__ == '__main__' :
 	print('Loading trajectories...')
--- waters.py
+++ waters.py
@@ -1,20 +1,37 @@
+'''
+Calculates the average number of water contacts for each residue across the trajectory.
+For each residue the waters are selected in a specified cutoff distance and then the counts are averaged
+for that residue.
+
+Output:
+a .csv file with indexed by resid including residue names and the average water contacts
+e.g.
+resid | resname | waters
+    1       LEU |    2.6 
+
+Running the script
+python waters.py MDAnalysis_supported_topolgy MDAnalysis_supported_trajectory csv_output_name.csv
+e.g.
+python contacts.py FXN_R165N_complex.prmtop FXN_R165N_complex1.dcd waters1.csv
+'''
 import sys
 import MDAnalysis
 import pandas as pd
 from pandas import DataFrame
-import numpy as np
+import numpy as np # numpy 1.16.6
 from MDAnalysis.analysis import distances
 import multiprocessing
 
-
+'''
+Load trajectory into curr_traj
+'''
 def getTraj():
 	global dcd
-	global curr_traj
+	global curr_traj # must be global to work with multiprocessing
 	top = str(sys.argv[1])
 	dcd = str(sys.argv[2])
 	traj = MDAnalysis.Universe(top, dcd)
 	curr_traj = traj	
-	return traj
 
 
 def get_res_waters(traj,cutoff=2):
@@ -29,28 +46,35 @@ def get_res_waters(traj,cutoff=2):
 	return pd.DataFrame(ret,columns=('resid','waters'))
 
 
+'''
+calls get_res_contacts and returns numpy area of contacts for that frame
+'''
 def traj_frame_waters(i):
         curr_traj.trajectory[i]
         rc = get_res_waters(curr_traj)
         return rc.waters.to_numpy()
 
 
+'''
+Creates and returns the final dataframe with water contacts averaged for each residue over the
+entire trajectory
+'''
 def waters_df():
  	waters = pd.DataFrame()
 	protein = curr_traj.select_atoms('protein')
 	waters['resid'] = protein.residues.resids
 	waters['resname'] = protein.residues.resnames
- 	print("Calculating waters for traj_%s" % dcd.split('_')[-1][0])
+ 	print("Calculating waters...")
 	pool = multiprocessing.Pool()
     	traj_waters = pool.map(traj_frame_waters,range(curr_traj.trajectory.n_frames))
     	traj_waters = np.array(traj_waters)
-	waters['traj_{}'.format(dcd.split('_')[-1][0])] = (traj_waters.mean(axis=0))
+	waters['waters'] = (traj_waters.mean(axis=0))
    	return waters.set_index('resid')
 
 
 if __name__ == '__main__' :
 	print('Loading trajectories...')
-	traj = getTraj()
+	getTraj()
 	print('Calculating waters...')
 	waters = waters_df()
 	print('Outputing csv...')
