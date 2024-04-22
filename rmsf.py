f8aa42a31c86b390f0d6310a8eb18831d39ee195
David Koes <dkoes@pitt.edu>
2024-04-22 09:18:21 -0400

    update rmsf

--- rmsf.py
+++ rmsf.py
@@ -1,114 +1,54 @@
+#!/usr/bin/env python3
+
+'''Calculate RMSF of specified trajectory or trajectories (all with same topology).
+   Outputs csv file and annotated structure.
 '''
-Calculates the average RMSF for each residue in the trajectory using
-MDAnalysis's RMSF method.
-
-Output:
-If called using shortcut for multiple residues, returned is a single csv called rmsfs.csv containg
-the resname, resid, and trajectory number
-e.g.
-resname | resid | traj_1 | resid | traj_2 ...
-LEU     |     1 |  1.272 |     1 |  1.410
-...
-
-If called using a single trajectory, returned is a csv called rmsfs.csv containing resid, resname
-and RMSF
-
-resid | resname | RMSF
-    1 | LEU     | 1.27
-...
-
-Running the script:
-Since calculating RMSF is not a very intense calculation, it may be useful to just
-produce a single csv for all trajectories. If your trajetories are .dcd and have 
-a naming scheme of prefix_number.dcd e.g. traj_1.dcd you can use the shortcut call.
-
-python rmsf.py MDAnalysis_supported_topology trajectory_prefix total_number_of_trajs_starting_at_1
-e.g. 
-python rmsf.py FXN_R165N_complex.prmtop FXN_R165N_compex_ 10 
-
-You can also call the script using a single trajectory:
-
-python rmsfs.py MDAnalysis_supported_topology MDAnalysis_supported_trajectory
-e.g.  
-python rmsf.py FXN_R165N_complex.prmtop FXN_R165N_compex_1.dcd
-'''
-import sys
-import MDAnalysis
+import MDAnalysis as mda
+import argparse
+from MDAnalysis.analysis import rms, align
+import MDAnalysis.analysis
+import os
 import pandas as pd
-from pandas import DataFrame
-import numpy as np # numpy 1.16.6
-from MDAnalysis.analysis import align
-from MDAnalysis.analysis.rms import RMSF
+from os.path import splitext, basename
 
-'''
-Loads trajectory(s) into trajs
-'''
-def getTrajs():
-	top = str(sys.argv[1])
-	dcd_prefix = str(sys.argv[2])
+parser = argparse.ArgumentParser(description='Identify most distinct frames of md trajectory\nIMPORTANT: assumes an aligned input')
+parser.add_argument("topology",required=True,help='Topology file')
+parser.add_argument("trajectory", nargs='+',required=True,'Trajectory file(s)')
+parser.add_argument("--selection",default="backbone",required=False,'Selection to align to, default backbone')
+parser.add_argument("--out",default=None,required=False,help='Output prefix for csv and pdb file')
 
-	trajs = [];
+args = parser.parse_args()
 
-	if (len(sys.argv) == 4):
-		dcd_total = int(sys.argv[3])
-		for i in range(dcd_total):
-    			trajs.append(MDAnalysis.Universe(top, dcd_prefix + '{}.dcd'.format(i + 1)))
-    		traj = MDAnalysis.Universe(top, [dcd_prefix + '{}.dcd'.format(i + 1) for i in range(10)])
-	else:
-		trajs.append(MDAnalysis.Universe(top, dcd_prefix))
-		traj = trajs[0]
+U = mda.Universe(args.topology, args.trajectory)
 
-	return trajs, traj
+#strip water
+notwat = U.select_atoms('not resname WAT')
+u = mda.Merge(notwat).load_new(
+         AnalysisFromFunction(lambda ag: ag.positions.copy(), notwat).run().results['timeseries'],
+         format=MemoryReader)
 
+#find average structure
+average = align.AverageStructure(u, u, select=args.selection, ref_frame=0).run()
+ref = average.results.universe
 
-'''
-Calculates the avergae RMSF for each residue in the trajectory
-'''
-def get_rmsf(t,label=None):
-	sel = 'protein and not name H*'
-	prot = t.select_atoms(sel)
-    	average_coordinates = t.trajectory.timeseries(asel=prot).mean(axis=1)
-    	# make a reference structure (need to reshape into a 1-frame "trajectory")
-    	reference = MDAnalysis.Merge(prot).load_new(average_coordinates[:, None, :], order="afc")
-    	aligner = align.AlignTraj(t, reference, select=sel, in_memory=True).run()
-    	print('\tDone Aligning... calculating RMSF')
-    	rmsfer = RMSF(prot).run()
-    	ret = pd.DataFrame(zip(prot.resids,prot.resnames,rmsfer.rmsf),columns=('resid','resname','rmsf'))
-    	if label != None:
-        	ret['label'] = label
-    	return ret
+# align to this structure
+aligner = align.AlignTraj(u, ref, select=args.selection, in_memory=True).run()
 
+sel = u.select_atoms(args.selection)
+R = rms.RMSF(sel).run()
 
-'''
-Creates and returns the final dataframe to become rmsfs.csv
-'''
-def rmsf_df(trajs, traj):
- 	rmsfs = DataFrame()
- 	protein = traj.select_atoms('protein')
+if args.out == None:
+    prefix = splitext(args.topology)[0]+'.rmsf'
+else:
+    prefix = args.out
+    
+res = zip(sel.indices,sel.resids,sel.resnames,R.results['rmsf'])
+df = pd.DataFrame(res,columns=('index','resid','resname','rmsf'))
+df.to_csv(f'{prefix}.csv')
 
- 	rmsfs['resname'] = protein.residues.resnames
-
- 	for i in range(len(trajs)):
-		print('\tCalculating rmsf for traj {}'.format(i + 1))
-    		rmsf = get_rmsf(trajs[i])
-		if (len(trajs) == 1):
-			col_name = 'RMSF'
-		else:
-    			col_name = 'traj_%s' % str(i + 1)
-    		rmsf = rmsf.groupby(['resid']).mean().reset_index().rename(columns={'rmsf': col_name})
-    		rmsfs = pd.concat([rmsfs, rmsf], axis=1)
-   	if (len(trajs) == 1):
-		return rmsfs.set_index('resid')
-	else:
-		return rmsfs
-
-
-if __name__ == '__main__' :
-	print('Loading trajectories...')
-	trajs, traj = getTrajs()
-	print('Calculating rmsfs')
-	rmsfs = rmsf_df(trajs, traj)
-	print('Outputing csv...')
-	rmsfs.to_csv('rmsfs.csv')
-	print('Done')
-
+u.add_TopologyAttr('tempfactors') # add empty attribute for all atoms
+u.trajectory[0]
+for residue, r_value in zip(sel.residues, R.results.rmsf):
+    residue.atoms.tempfactors = r_value
+    
+u.atoms.write(f'{prefix}.pdb')
